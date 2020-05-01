159793b857f7e8d6f4aa30af3f91148cda460ae7
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-05-01 10:27:41 +0530

    Connect to db.

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+// Load env vars
+dotenv.config({
+  path: './config/config.env',
+});
+
+// Connect to DB
+connectDB();
 
 const app = express();
 
+// Use body-parser
+app.use(express.json());
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}!`);
+});
+
+server.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+
+  server.close(() => process.exit());
 });
